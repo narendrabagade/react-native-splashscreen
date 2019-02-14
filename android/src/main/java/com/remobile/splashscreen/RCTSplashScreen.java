5d2f72dfbe0c9986608322d591055e058e15d0bf
Narendra Bagade <narendra.bagade@copart.com>
2019-02-13 21:34:54 -0600

    Update RCTSplashScreen.java

--- android/src/main/java/com/remobile/splashscreen/RCTSplashScreen.java
+++ android/src/main/java/com/remobile/splashscreen/RCTSplashScreen.java
@@ -92,9 +92,18 @@ public class RCTSplashScreen extends ReactContextBaseJavaModule {
     }
 
     private int getSplashId() {
-        int drawableId = getActivity().getResources().getIdentifier("splash", "drawable", getActivity().getClass().getPackage().getName());
+         int drawableId = getActivity().getResources().getIdentifier("splash", "drawable", getActivity().getClass().getPackage().getName());
+        final int orientationInt = getReactApplicationContext().getResources().getConfiguration().orientation;
+        if(orientationInt == 2){
+            drawableId = getActivity().getResources().getIdentifier("splash_land", "drawable", getActivity().getClass().getPackage().getName());
+        }
         if (drawableId == 0) {
-            drawableId = getActivity().getResources().getIdentifier("splash", "drawable", getActivity().getPackageName());
+            if(orientationInt == 2){
+                drawableId = getActivity().getResources().getIdentifier("splash_land", "drawable", getActivity().getPackageName());
+            } else {
+                drawableId = getActivity().getResources().getIdentifier("splash", "drawable", getActivity().getPackageName());
+            }
+
         }
         return drawableId;
     }
